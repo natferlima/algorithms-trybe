4d539a703ce737a4106bde432c6c4ab379f6ba93
natferlima <ferlima.natalia@gmail.com>
2022-07-10 17:58:00 -0300

    feat: cria função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """  Faça o código aqui.  """
+    num_students = 0
+    try:
+        for entry, exit in permanence_period:
+            if target_time >= entry and target_time <= exit:
+                num_students += 1
+    except TypeError:
+        return None
+    return num_students
